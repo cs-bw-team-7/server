564a3f86534809b2a164a412b965daf3fe3188f3
Arron M <marshall.arron@gmail.com>
2019-10-21 16:04:39 -0700

    Add auth middleware to check token

---
+++ api/middleware/auth.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const log = require('../../utils/logger');
+
+const auth = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const token = authorization.replace('Token ', '');
+    if (!authorization) return res.status(400).json({
+      status: 'error',
+      error: 'MissingAuth',
+      message: 'Please provide a token in the `Authorization` header.',
+    });
+
+    // who are you?
+    if (token !== process.env.ARRON_TOKEN || token !== process.env.MEGAN_TOKEN) return res.status(400).json({
+      status: 'error',
+      error: 'InvalidToken',
+      message: 'Unfortunately the server does not know the provided token.',
+    });
+
+    req.token = token;
+
+    next();
+  } catch (error) {
+    res.status(500).json(await log.err(error));
+  }
+}
+
+module.exports = auth;
