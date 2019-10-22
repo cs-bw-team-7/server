ca4fd6beb1c5739fe9e0b134bb2fe5fd8bdb555b
Arron M <marshall.arron@gmail.com>
2019-10-21 21:02:57 -0700

    Update coords so it converts to int and exports properly

--- utils/coordsToArray.js
+++ utils/coordsToArray.js
@@ -1,11 +1,13 @@
 /*
-TODO: Future docs
+TODO: Future Docs
 Simple util to convert from coord string to array with x, y keys
 */
 const coordsToArray = string => {
   const coordsArray = string.replace(/\(|\)/g, '').split(',');
   return {
-    x: coordsArray[0],
-    y: coordsArray[1],
+    x: Number(coordsArray[0]),
+    y: Number(coordsArray[1]),
   };
 };
+
+module.exports = coordsToArray;
